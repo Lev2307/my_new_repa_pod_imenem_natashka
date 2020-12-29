82e3846f80cc80b3468779933f1c973360b4b27c
Lev2307 <levchik23072007@gmail.com>
2020-12-29 18:22:48 +0300

    avatar beta

--- soup.py
+++ soup.py
@@ -1,8 +1,9 @@
+import sys
 import requests
 
 from bs4 import BeautifulSoup
 
-query = 'cats'
+query = sys.argv[1] if len(sys.argv) > 1 else input("Введите тип вашего аватара: ")
 
 url = f"https://www.kiddle.co/s.php?q={query}"
 
@@ -17,4 +18,7 @@ for raw_img in soup.find_all('img'):
         response = requests.get(link)
         with open("./today_avatar.jpg", 'wb') as f:
             f.write(response.content)
+        print('Аватар найден - avatar_today.jpg')
         break
+else:
+    print('Аватар не найден')
